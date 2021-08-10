5773e464b6131227fed592c86ff3a4487404a199
Rajit shyaula <rshyaula23@gmail.com>
2021-08-10 23:37:25 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+var tomr ='sunny';
+
+if(tomr =='rain'){
+    console.log('take a raincoat');
+
+}else {
+    console.log('no need totake a raincoat');
+
+}
+
+
+ var year = 2020;
+ if(year % 4 == 0){
+     if(year % 100 ==0){
+         if(year % 400 ==0){
+             console.log("The year" +year + "is a leap year");
+         }else{
+             console.log("The year" + year + "is not a leap year")
+         }
+
+     }else{
+         console.log("The year" + year + " is not a leap year");
+     }
+
+ }else{
+     console.log("The year" + year + " is not a leap year");
+ }
+
